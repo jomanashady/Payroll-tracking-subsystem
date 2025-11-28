0ccccdd86e182e7785f9a0d7a5776f9301b81657
Hassan Osama <73666211+hassanosama5@users.noreply.github.com>
2025-11-28 16:37:46 +0200

    Remove subtree integration and employee-module folder

--- employee-module/src/organization-structure/organization-structure.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { OrganizationStructureService } from './organization-structure.service';
-import { OrganizationStructureController } from './organization-structure.controller';
-
-@Module({
-  providers: [OrganizationStructureService],
-  controllers: [OrganizationStructureController]
-})
-export class OrganizationStructureModule {}
